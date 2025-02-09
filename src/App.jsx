5b191f6239767b1d66a65d2c213d8677e54e5be8
sayudha <mazaya.studio@outlook.com>
2025-02-09 22:07:11 +0700

    Add JSON Server and enhance Sidebar with Outlet support
    Introduced `json-server` for simulating backend API interactions, including a new `server` script in `package.json`. Updated `Sidebar` component to use React Router's `Outlet`, allowing nested routing. Minor class name adjustment in `Map` component for better clarity.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [cities, setCitites] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const BASE_URL = "http://localhost:8000"
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCitites(data)
+      } catch {
+        alert("Something went wrong")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchCities();
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +39,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path='cities' element={<CityList />} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path='countries' element={<p>Countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,14 @@
 import styles from './CityList.module.css';
-function CityList() {
+import Spinner from "./Spinner.jsx";
+import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
+
+function CityList({cities, isLoading}) {
+  if (isLoading) return <Spinner />
+  if (!cities.length) return <Message message="No cities found" />
   return (
     <ul className={styles.cityList}>
-    testeas
+      {cities.map(city => <CityItem key={city.id} city={city} />)}
     </ul>
   )
 }
