0896ce81fd0d1d7509044585b4e9c4e40ea25049
sayudha <mazaya.studio@outlook.com>
2025-02-09 20:58:30 +0700

    Add navigation bar and logo across main pages
    Introduced a reusable `PageNav` component with improved styling and integrated a clickable logo for consistent navigation. Updated the layout of multiple pages to include the navigation bar and added a new `Login` route to the app. Enhanced the `AppNav` styles for better design consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
